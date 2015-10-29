27a95c0bd639b8f87a76c850dea8af89d0497f4c
Thierry Hubmann <thierry.hubmann@he-arc.ch>
2015-10-29 10:43:57 +0100

    Renommage des classes beans au singulier

---
+++ src/java/Model/Achievement.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author JulmyS
+ */
+public class Achievement {
+    private int numero;
+    private String libelle;
+
+    public Achievement(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    
+    
+}
