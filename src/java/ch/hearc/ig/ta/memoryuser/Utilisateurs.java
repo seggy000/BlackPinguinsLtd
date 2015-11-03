6921c56ae3136cfb3b487ed9d8e131ff8d6363a4
Thierry Hubmann <thierry.hubmann@he-arc.ch>
2015-11-03 10:51:04 +0100

    Modification de la méthode de login pour que la vérification se fasse dans la base de données

--- src/java/ch/hearc/ig/ta/memoryuser/Utilisateurs.java
+++ src/java/ch/hearc/ig/ta/memoryuser/Utilisateurs.java
@@ -4,6 +4,7 @@
  */
 package ch.hearc.ig.ta.memoryuser;
 
+import ch.hearc.ig.ta.dao.CommerciauxDAO;
 import java.util.Hashtable;
 
 /**
@@ -11,24 +12,38 @@ import java.util.Hashtable;
  * @author francill
  */
 public class Utilisateurs {
-    public static Hashtable<String,String>  users =null;
 
-    public static boolean checkPassword(String user, String pw){
-        return true;
+    public static Hashtable<String, String> users = null;
+
+    public static boolean checkPassword(String user, String pw) {
+        if (!user.equals(null)) {
+            CommerciauxDAO dao = new CommerciauxDAO();
+            String password = dao.returnPasswordByUsername(user);
+            return password.equals(pw);
+        } else {
+            return false;
+        }
     }
-    public static boolean verifyUser(String user, String pw){
-        if(users == null){
-            users = new Hashtable<String,String>();
-            users.put("termine", "ptermine");
-            users.put("daucourt","pdaucourt");
-            users.put("francillon","pfrancillon");
-            users.put("dupont","pdupont");
-            users.put("t","t");
-        }     
-        System.out.println(user + " " + pw +" " +users.get(user));
-        if(users.get(user)!=null){
-            if ( users.get(user).equals(pw) ) return true;else return false;
-        }else return false;
-        
-    }
+//
+//    public static boolean verifyUser(String user, String pw) {
+//        if (users == null) {
+//            users = new Hashtable<String, String>();
+//            users.put("termine", "ptermine");
+//            users.put("daucourt", "pdaucourt");
+//            users.put("francillon", "pfrancillon");
+//            users.put("dupont", "pdupont");
+//            users.put("t", "t");
+//        }
+//        System.out.println(user + " " + pw + " " + users.get(user));
+//        if (users.get(user) != null) {
+//            if (users.get(user).equals(pw)) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } else {
+//            return false;
+//        }
+//
+//    }
 }
