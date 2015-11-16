93d1e5f9f34dc8afdf635ffa53e89ff424d09818
Geoffroy Megert <geoffroy.megert@he-arc.ch>
2015-11-16 22:33:38 +0100

    Ajout du fichier SQL de peuplement de la table Achievements.

---
+++ TA_Peuplement_achievements.sql
@@ -0,0 +1,16 @@
+INSERT INTO achievements (libelle)
+  VALUES ('Première connexion');
+
+INSERT INTO achievements (libelle)
+  VALUES ('Premier client');
+
+INSERT INTO achievements (libelle)
+  VALUES ('Première suppression');
+
+INSERT INTO achievements (libelle)
+  VALUES ('Première modification');
+
+INSERT INTO achievements (libelle)
+  VALUES ('Premier coup d''oeil');
+
+COMMIT;
