1aafa54fee32b93dc326e3691ff76ebeed92a23a
Geoffroy Megert <geoffroy.megert@he-arc.ch>
2015-11-27 14:20:05 +0100

    Mise à jour des données sur la BD.

--- TA_Peuplement_achievements.sql
+++ TA_Peuplement_achievements.sql
@@ -1,34 +1,37 @@
-INSERT INTO achievements (libelle)
-  VALUES ('Première connexion');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Première connexion', 'Vous vous êtes connecté pour la première fois à BlackPinguinsLtd.');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Premier client');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Premier client', 'Vous avez enregistré votre premier client !');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Première suppression');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Première suppression', 'Malheureusement, vous avez dû vous séparer d''un client pour la première fois. Courage, perceverez !');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Première modification');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Première modification', 'Vous avez mis à jour les données d''un client pour la première fois. Un client écouté est un client heureux !');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Premier coup d''oeil');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Premier coup d''oeil', 'Vous avez affiché la liste de vos clients pour la première fois. Gardez cette habitude, le suivi des clients est important !');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Niveau 1 atteint !');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Débutant atteint !', 'Vous possédez un compte dans BlackPinguinsLtd.');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Niveau 5 atteint !');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Initié atteint !', 'Vous avez dépasser les 500 points.');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Niveau 10 atteint !');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Professionnel atteint !', 'Vous avez dépasser les 1''000 points.');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Niveau 15 atteint !');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Connaisseur atteint !', 'Vous avez dépasser les 1''500 points.');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Niveau 25 atteint !');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Confirmé atteint !', 'Vous avez dépasser les 2''500 points.');
 
-INSERT INTO achievements (libelle)
-  VALUES ('Niveau 50 atteint !');
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Expert atteint !', 'Vous avez dépasser les 5''000 points.');
+
+INSERT INTO achievements (libelle, description)
+  VALUES ('Niveau Légendaire atteint !', 'Vous avez dépasser les 10''000 points !!!');
   
 COMMIT;
